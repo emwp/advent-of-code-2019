9f3807cc6b8c9df2cdf130c350ab13c5b31ab981
Everton Pereira <everton.mwp@gmail.com>
2019-12-08 20:51:22 -0300

    Add Day3 Part2

--- typescript/src/day-3/day3.ts
+++ typescript/src/day-3/day3.ts
@@ -1,3 +1,6 @@
+// Instructions:
+// https://adventofcode.com/2019/day/3
+
 import { readInput } from "../../utils/readInput"
 
 interface Wire {
@@ -5,9 +8,16 @@ interface Wire {
   Distance: number
 }
 
-interface Paths {
+interface Path {
   X: number
   Y: number
+  Steps: number
+}
+
+interface Intersection {
+  Point: Path
+  WireA: Path
+  WireB: Path
 }
 
 const wiresInput = (input: string) => {
@@ -23,32 +33,36 @@ const firstWire = wiresInput(readInput())[0]
 const secondWire = wiresInput(readInput())[1]
 
 const getPaths = (wire: Wire[]) => {
-  const visitedPaths: Paths[] = []
-  const currentPath: Paths = { X: 0, Y: 0 }
+  const visitedPaths: Path[] = []
+  const currentPath: Path = { X: 0, Y: 0, Steps: 0 }
 
   wire.forEach(step => {
     switch (step.Direction) {
       case "R":
         for (let i = 0; i < step.Distance; i++) {
           currentPath.X += 1
+          currentPath.Steps += 1
           visitedPaths.push({ ...currentPath })
         }
         break
       case "L":
         for (let i = 0; i < step.Distance; i++) {
           currentPath.X -= 1
+          currentPath.Steps += 1
           visitedPaths.push({ ...currentPath })
         }
         break
       case "U":
         for (let i = 0; i < step.Distance; i++) {
           currentPath.Y += 1
+          currentPath.Steps += 1
           visitedPaths.push({ ...currentPath })
         }
         break
       case "D":
         for (let i = 0; i < step.Distance; i++) {
           currentPath.Y -= 1
+          currentPath.Steps += 1
           visitedPaths.push({ ...currentPath })
         }
         break
@@ -57,29 +71,38 @@ const getPaths = (wire: Wire[]) => {
   return visitedPaths
 }
 
-const intersections = (points1: Paths[], points2: Paths[]) => {
-  const intersections: Paths[] = []
-  points1.forEach((point1, i) => {
-    console.log(`${i} / ${points1.length}`)
-    points2.forEach(point2 => {
-      if (point1.X == point2.X && point1.Y == point2.Y) {
-        intersections.push(point1)
+const intersections = (pointsA: Path[], pointsB: Path[]): Intersection[] => {
+  const intersections: Intersection[] = []
+  pointsA.forEach((pointA, i) => {
+    console.log(`${i} / ${pointsA.length} - Loading!?`)
+    pointsB.forEach(pointB => {
+      if (pointA.X == pointB.X && pointA.Y == pointB.Y) {
+        intersections.push({ Point: pointA, WireA: pointA, WireB: pointB })
       }
     })
   })
   return intersections
 }
 
-const getClosestDistance = (intersections: Paths[]) => {
+const getClosestDistance = (intersections: Intersection[]) => {
   const distance: number[] = []
-  intersections.forEach(point => {
-    const pointDistance = Math.abs(point.X) + Math.abs(point.Y)
+  let currentMinSteps: number
+  intersections.forEach(item => {
+    const pointDistance = Math.abs(item.Point.X) + Math.abs(item.Point.Y)
     distance.push(pointDistance)
-  })
 
+    const IntersectionStepCount = item.WireA.Steps + item.WireB.Steps
+    if (!currentMinSteps || IntersectionStepCount < currentMinSteps) {
+      currentMinSteps = IntersectionStepCount
+    }
+  })
   const minDistance = Math.min.apply(null, distance)
-  return minDistance
+
+  return {
+    MinDistance: minDistance,
+    MinSteps: currentMinSteps
+  }
 }
 
 const intersecPoints = intersections(getPaths(firstWire), getPaths(secondWire))
-console.log('MINIMUN DISTANCE: ', getClosestDistance(intersecPoints))
+console.log(getClosestDistance(intersecPoints))
