5b56fc135dc57e88ec7c891673fb25ad7b37eac7
Everton Pereira <everton.mwp@gmail.com>
2019-12-08 01:04:45 -0300

    Add solution day2 part 2

--- typescript/src/day-2/day2.ts
+++ typescript/src/day-2/day2.ts
@@ -1,3 +1,4 @@
+// Instructions:
 // https://adventofcode.com/2019/day/2
 
 import { readInput } from "../../utils/readInput"
@@ -10,7 +11,7 @@ const intCode = (input: number[]) => {
   const stepSize = 4
 
   for (position; position < input.length; position + stepSize) {
-    if (input[position] === 99) return input
+    if (input[position] === 99) return input[0]
     const currentValue = input.slice(position, position + stepSize)
     switch (currentValue[0]) {
       case 1:
@@ -28,11 +29,33 @@ const intCode = (input: number[]) => {
         )
         break
       default:
-        return input
+        return input[0]
     }
     position = position + stepSize
   }
-  return input
 }
 
-intCode(dataInput)
+const checkNounVerb = (list: number[], noun: number, verb: number, target: number) => {
+  const initialList = [...list]
+  initialList.splice(1, 1, noun)
+  initialList.splice(2, 1, verb)
+  const result = intCode(initialList) 
+  return result === target ? result : false 
+}
+
+const findNounAndVerb = (input: number[], target: number) => {
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      const isTarget = checkNounVerb(input, i, j, target)
+      if (isTarget === target) {
+        return {
+          noun: i,
+          verb: j,
+          result: 100 * i + j
+        }
+      }
+    }
+  }
+}
+
+findNounAndVerb(dataInput, 19690720)
