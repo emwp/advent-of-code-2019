76baa410d210554ce9a79a214eaabfac5b4136b4
Everton Pereira <everton.mwp@gmail.com>
2019-12-09 00:20:21 -0300

    Add Day4 Part2

--- typescript/src/day-4/day4.ts
+++ typescript/src/day-4/day4.ts
@@ -1,30 +1,39 @@
+// Instructions:
+// https://adventofcode.com/2019/day/4
+
 import { readInput } from "../../utils/readInput"
 
 const rawInput = readInput()
 const dataInput = rawInput.split("-").map(Number)
 
-const findPasswordRange = (input : number[]) => {
+const findPasswordRange = (input: number[]) => {
   let range = 0
   for (let i = input[0]; i <= input[1]; i++) {
     const currentPassArray = Array.from(i.toString()).map(Number)
     const passSize = currentPassArray.length
 
-    let possiblePass = true
+    let possiblePassword = true
     let hasDoubles = false
+    let doubleSetLargerThanTwo = true
 
     for (let j = 0; j < passSize; j++) {
       if (currentPassArray[j] < currentPassArray[j - 1]) {
-        possiblePass = false
+        possiblePassword = false
         break
       }
 
       if (currentPassArray[j - 1] === currentPassArray[j]) {
         hasDoubles = true
+        const doubleDigit = currentPassArray[j]
+        
+        if (currentPassArray[j + 1] !== doubleDigit && currentPassArray[j - 2] !== doubleDigit) {
+          doubleSetLargerThanTwo = false
+        }
       }
     }
-    if (hasDoubles && possiblePass) {
+    if (hasDoubles && possiblePassword && !doubleSetLargerThanTwo) {
       range++
-    } 
+    }
   }
   return range
 }
